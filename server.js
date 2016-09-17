31717fc42e60bb3fa304b9803ffb87d646bcaf8b
Pashupati Nath Verma <verma2471995@gmail.com>
2016-09-17 22:54:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/article-one', function (req, res) {
+  res.send("this is server 1");
+});
+
+app.get('/article-two', function (req, res) {
+  res.send("this is server 2");
+});
+
+app.get('/article-three', function (req, res) {
+  res.send("this is server 3");
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
