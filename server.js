eceb34737a5416cd1abc3cc4905b57446e1d6199
Pashupati Nath Verma <verma2471995@gmail.com>
2016-10-23 13:28:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var config=
 {
     user: 'pashupati123',
     database: 'pashupati123',
-    host: 'db.imad.hausra-app.io',
+    host: 'db.imad.hasura-app.io',
     port: '5432',
     password:process.env.DB_PASSWORD
 };
