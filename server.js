68ac1d405a2af2305b54f5548c7541bc81089763
Pashupati Nath Verma <verma2471995@gmail.com>
2016-10-23 22:19:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -144,6 +144,25 @@ app.get('/', function (req, res) {
           }
       }
      });
+   pool.query("SELECT * FROM article-2 WHERE title= $1", [req.params.articleName],function(err,result)
+     {
+      if(err)
+      {
+          res.status(500).send(err.toString());
+      }
+      else
+      {
+          if(result.rows.length===0)
+          {
+           res.status(404).send('article not found');
+          }
+          else
+          {
+              var articleData=result.rows[0];
+              res.send(createtemplete(articleData));
+          }
+      }
+     });
   
 });
 
