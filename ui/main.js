34ce6e9b9dc9c8a73b764c506b965b1d6946ba2f
Pashupati Nath Verma <verma2471995@gmail.com>
2016-10-24 14:11:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,10 @@
-console.log('Loaded!');
+var button=document.getElementByid("counter");
+var counter=0;
+
+button.onclick=function()
+{
+  counter=counter+1;
+  var span=document.getElementByid("count");
+  span.innerHtml=counter.toString();
+  
+};
