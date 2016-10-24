81c0ee0de2ebe654a0be2c6b5bcf099d6daace6e
Pashupati Nath Verma <verma2471995@gmail.com>
2016-10-24 14:29:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,24 @@
 var button=document.getElementById('counter');
-var counter=0;
 
 button.onclick=function()
 {
-  counter=counter+1;
-  var span=document.getElementById('count');
-  span.innerHtml=counter.toString();
+  var request=new XMLHttpRequest();
+  request.onreadystatechange =functon()
+  {
+      if(request.readyState===XMLHttpRequest.DONE)
+      {
+         if(request.status===200)
+         {
+             var counter=request.responseText;
+             var span=document.getElementById('count');
+             span.innerHtML=ccounter.toString();
+             
+         }
+      }
+  }
+  
+  request.open('GET','http://pashupati123.imad.hasura-app.io/counter',true);
+  request.send(null);
+  
   
 };
