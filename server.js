3c1c82f88985e52353b6afd0260205ed9455906a
Pashupati Nath Verma <verma2471995@gmail.com>
2016-10-28 01:13:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -79,6 +79,126 @@ return htmltemplete;
 }
 
 
+function createtemplete_resume(data)
+{
+    var title=data.title;
+    var date=data.date;
+    var heading=data.heading;
+    var content=data.content;
+var htmltemplete_resume=
+
+    `<html>
+    <head>
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="/ui/style.css" rel="stylesheet" />
+      <title>
+          ${title}
+      </title> 
+     
+    </head>
+    <body background="http://www.walldevil.com/wallpapers/a24/8861-background-color-ground-back-patterns.jpg">
+        <div  class="container">
+         <nav>
+                <a style="underline=none" href="/">Home</a>
+                
+                <!--a href="article-two">About&nbsp;&nbsp;&nbsp;&nbsp;</a-->
+                
+               
+           </nav>
+          <hr/>
+            <!--div>
+                <img src="http://impreza.us-themes.com/wp-content/uploads/img-6.jpg"/>
+            </div-->
+       <div>
+          
+        </div>
+        
+           
+           <h1>
+           ${heading}
+           </h1>
+           <div>
+           ${date.toDateString()}
+           </div>
+           <div class="container">
+             ${content}
+               <div class="footer">
+                
+                 <hr/>
+                <input type="text" id= "name" placeholder="Type Comment"></input>
+                <input type="submit" value="comment" id="submit_btn"></input>
+                <ul id="namelist">
+                    
+                </ul>
+            </div>
+           
+          </div>
+      
+       </div>
+    </body>
+</html>
+`;
+return htmltemplete_resume;
+}
+
+
+function createtemplete_contact(data)
+{
+    var title=data.title;
+    var date=data.date;
+    var heading=data.heading;
+    var content=data.content;
+var htmltemplete_contact=
+
+    `<html>
+    <head>
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="/ui/style.css" rel="stylesheet" />
+      <title>
+          ${title}
+      </title> 
+     
+    </head>
+    <body background="http://www.walldevil.com/wallpapers/a24/8861-background-color-ground-back-patterns.jpg">
+        <div  class="container">
+         <nav>
+                <a style="underline=none" href="/">Home</a>
+                
+                <!--a href="article-two">About&nbsp;&nbsp;&nbsp;&nbsp;</a-->
+                
+               
+           </nav>
+          <hr/>
+            <!--div>
+                <img src="http://impreza.us-themes.com/wp-content/uploads/img-6.jpg"/>
+            </div-->
+       <div>
+          
+        </div>
+        
+           
+           <h1>
+           ${heading}
+           </h1>
+           <div>
+           ${date.toDateString()}
+           </div>
+           <div class="container">
+             ${content}
+              
+            </div>
+           <hr/>
+          </div>
+      
+       </div>
+    </body>
+</html>
+`;
+return htmltemplete_contact;
+}
+
+
+
 function createtemplete_blog(data)
 {
     var title=data.title;
@@ -320,6 +440,60 @@ app.get('/submit-name', function(rq,res)
   
   
 });
+
+
+app.get('/contact/:contactName', function (req, res) {
+     //var articleName=req.params.articleName;
+     //pool.query("SELECT * FROM article WHERE title= '" + req.params.articleName + "'",function(err,result)
+      pool.query("SELECT * FROM contact WHERE title= $1", [req.params.contactName],function(err,result)
+     {
+      if(err)
+      {
+          res.status(500).send(err.toString());
+      }
+      else
+      {
+          if(result.rows.length===0)
+          {
+           res.status(404).send('article not found');
+          }
+          else
+          {
+              var contactData=result.rows[0];
+              res.send(createtemplete_contact(contactData));
+          }
+      }
+     });
+});
+
+
+app.get('/resume/:resumeName', function (req, res) {
+     
+      pool.query("SELECT * FROM resume WHERE title= $1", [req.params.resumeName],function(err,result)
+     {
+      if(err)
+      {
+          res.status(500).send(err.toString());
+      }
+      else
+      {
+          if(result.rows.length===0)
+          {
+           res.status(404).send('article not found');
+          }
+          else
+          {
+              var resumeData=result.rows[0];
+              res.send(createtemplete_resume(resumeData));
+          }
+      }
+     });
+     
+  
+  
+});
+
+
 
 app.get('/blogs/:blogName', function (req, res) {
      //var articleName=req.params.articleName;
