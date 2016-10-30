98c15abdb9abfa6b00437ef5dccd7f1a1f8831d6
Pashupati Nath Verma <verma2471995@gmail.com>
2016-10-30 14:37:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -59,6 +59,15 @@ var htmltemplete=
            </div>
            <div class="container">
              ${content}
+             <div class="footer">
+                
+                 <hr/>
+                <input type="text" id= "name" placeholder="Type Comment"></input>
+                <input type="submit" value="comment" id="submit_btn"></input>
+                <ul id="namelist">
+                    
+                </ul>
+            </div>
              <hr/>
              <div class="center">
     <p>
@@ -71,15 +80,7 @@ var htmltemplete=
     </p>
 </div>
 
-               <div class="footer">
-                
-                 <hr/>
-                <input type="text" id= "name" placeholder="Type Comment"></input>
-                <input type="submit" value="comment" id="submit_btn"></input>
-                <ul id="namelist">
-                    
-                </ul>
-            </div>
+               
            
           </div>
       
@@ -414,6 +415,19 @@ var htmltemplete_portfolio=
                     
                 </ul>
             </div>
+            <hr/>
+             <div class="center">
+    <p>
+          <a href="/"><b><font color="#000000" size="5">Home</font></b></a>&nbsp;&nbsp;
+   <a href="https://www.facebook.com/pashupati.verma.5" target="_blank"><b><font color="#000000" size="5">Facebook</font></b></a>&nbsp;&nbsp;
+     <a href="https://twitter.com/pashupativerma" target="_blank"><b><font color="#000000" size="5">Twitter</font></b></a>&nbsp;&nbsp;
+       <a href="https://www.linkedin.com/in/pashupatinathverma"><b><font color="#000000" size="5">LinkedIn</font></b></a>&nbsp;&nbsp;
+        <a href="https://aboutme.google.com/b/103594303224863671567/?_ga=1.107170242.1101935688.1477815173#profile_photo"><b><font color="#000000" size="5">Google+</font></b></a>
+   
+    </p>
+</div>
+
+        
           </div>
       
        </div>
