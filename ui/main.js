ee58ee7c44189e7a8b97b718e6dbb544275c1851
Pashupati Nath Verma <verma2471995@gmail.com>
2016-11-04 22:28:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,4 @@
+
 function loadLoginForm () {
     var loginHtml = `
         <h3>Login/Register to unlock awesome features</h3>
@@ -104,67 +105,35 @@ function loadLogin () {
     request.send(null);
 }
 
+function loadArticles () {
+        // Check if the user is already logged in
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            var articles = document.getElementById('articles');
+            if (request.status === 200) {
+                var content = '<ul>';
+                var articleData = JSON.parse(this.responseText);
+                for (var i=0; i< articleData.length; i++) {
+                    content += `<li>
+                    <a href="/articles/${articleData[i].title}">${articleData[i].heading}</a>
+                    (${articleData[i].date.split('T')[0]})</li>`;
+                }
+                content += "</ul>"
+                articles.innerHTML = content;
+            } else {
+                articles.innerHTML('Oops! Could not load all articles!')
+            }
+        }
+    };
+    
+    request.open('GET', '/get-articles', true);
+    request.send(null);
+}
 
 
-var button=document.getElementById('counter');
-
-button.onclick=function()
-{
-  var request=new XMLHttpRequest();
-  
- request.onreadystatechange = function(){
-      if(request.readyState===XMLHttpRequest.DONE)
-      {
-         if(request.status===200)
-         {
-             var counter=request.responseText;
-             var span=document.getElementById('count');
-             span.innerHTML=counter.toString();
-             
-         }
-      }
-  };
-  
-  request.open('GET','http://pashupati123.imad.hasura-app.io/counter',true);
-  request.send(null);
-  
-  
-};
-
-
-var submit=document.getElementById('submit_btn');
-submit.onclick = function()
-{
-    var request=new XMLHttpRequest();
-  
- request.onreadystatechange = function(){
-      if(request.readyState===XMLHttpRequest.DONE)
-      {
-         if(request.status===200)
-         {
-              var names=request.responseText;
-              names=JSON.parse(names);
-    var list='';
-    for(var i=0;i<names.length;i++)
-    {
-        list+='<li>'+names[i]+'</li>';
-    }
-    var ul=document.getElementById('namelist');
-    ul.innerHTML=list;
-         }
-      }
-  };
-  var nameInput=document.getElementById('name');
-  var name=nameInput.value;
-  request.open('GET','http://pashupati123.imad.hasura-app.io/submit-name='+name,true);
-  request.send(null);
-};
-
-
-document.getElementById("link").style.width=document.getElementById("img").width+"px";
-
+// The first thing to do is to check if the user is logged in!
 loadLogin();
 
-
-
-
+// Now this is something that we could have directly done on the server-side using templating too!
+loadArticles();
