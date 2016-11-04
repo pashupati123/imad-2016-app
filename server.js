1052bd32dbdf0b3f10413e0d289f897dafe8fcb5
Pashupati Nath Verma <verma2471995@gmail.com>
2016-11-04 21:49:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -495,7 +495,7 @@ app.get('/hash/:input', function(req, res) {
 
 app.post('/create-user', function (req, res) {
    // username, password
-   // {"username": "tanmai", "password": "password"}
+   // {"username": "pashupati", "password": "password"}
    // JSON
    var username = req.body.username;
    var password = req.body.password;
